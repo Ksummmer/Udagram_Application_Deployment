126f78258da7fc769deaf34f59d8a93ac5d517a6
Ksenya <ks_s_6@mail.ru>
2023-08-31 13:39:11 +0200

    Update2

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,12 +1,27 @@
-import { Sequelize } from "sequelize-typescript";
+import { Sequelize, SequelizeOptions  } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
+const options: SequelizeOptions = {
   username: config.username,
   password: config.password,
   database: config.database,
   host: config.host,
-
+  port: config.port,
   dialect: "postgres",
+  //native: true,
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false 
+    }
+  },
   storage: ":memory:",
-});
+  logging: console.log
+};
+
+console.log(config);
+
+
+export const sequelize = new Sequelize(`postgres://${config.username}:${config.password}@${config.host}:5432/${config.database}`, 
+options);
+
--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -18,32 +18,39 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
     console.error("Unable to connect to the database:", error);
   }
 
-  await sequelize.addModels(V0_FEED_MODELS);
-  await sequelize.addModels(V0_USER_MODELS);
-  await sequelize.sync();
+  try {
+    await sequelize.addModels(V0_FEED_MODELS);
+    await sequelize.addModels(V0_USER_MODELS);
+    console.log('Models were added');
+    await sequelize.sync({ force: true, logging: console.log });
+    console.log("Database connected");
+  }
+  catch (error) {
+    console.log(error)
+    console.error("Unable to connect to the database:", error);
+  }
 
-  console.log("Database Connected");
 
   const app = express();
   const port = 8080;
 
   app.use(bodyParser.json());
 
-  // app.use(cors());
+  app.use(cors());
   // We set the CORS origin to * so that we don't need to
   // worry about the complexities of CORS. 
-  app.use(cors({
-    "allowedHeaders": [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization', 'Access-Control-Allow-Origin',
-      'Access-Control-Allow-Headers',
-      'Access-Control-Allow-Methods'
-    ],
-    "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    "preflightContinue": true,
-    "origin": '*',
-  }));
+  // app.use(cors({
+  //   "allowedHeaders": [
+  //     'Origin', 'X-Requested-With',
+  //     'Content-Type', 'Accept',
+  //     'X-Access-Token', 'Authorization', 'Access-Control-Allow-Origin',
+  //     'Access-Control-Allow-Headers',
+  //     'Access-Control-Allow-Methods'
+  //   ],
+  //   "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  //   "preflightContinue": true,
+  //   "origin": '*',
+  // }));
 
   app.use("/api/v0/", IndexRouter);
 
