3ad061d65b9cfa3576f95d996884f0afa6eb0063
Ksenya <ks_s_6@mail.ru>
2023-09-02 21:40:16 +0200

    Update4

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://mybucket85585853942669/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://mybucket85585853942669/
+aws s3 cp --recursive --acl public-read ./www s3://mybucket85585853942669api/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://mybucket85585853942669api/
