847e76f8a6f539bae31d7517c751bcfbff46a5c4
Ksenya <ks_s_6@mail.ru>
2023-09-03 22:36:17 +0200

    Update20

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,6 +1,24 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
+// const options: SequelizeOptions = {
+  // username: config.username,
+  // password: config.password,
+  // database: config.database,
+  // host: config.host,
+  // port: config.port,
+  // dialect: "postgres",
+//   dialectOptions: {
+//     ssl: {
+//       require: true, 
+//       rejectUnauthorized: false 
+//     }
+//   },
+//   storage: ":memory:",
+//   logging: console.log
+// };
+
+// console.log(config);
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
@@ -8,11 +26,14 @@ export const sequelize = new Sequelize({
   host: config.host,
 
   dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-        "require": true,
-        "rejectUnauthorized": false
-    }
-  },
   storage: ":memory:",
+  dialectOptions: {
+        ssl: {
+          require: true, 
+          rejectUnauthorized: false 
+        }
+      }
 });
+
+// export const sequelize = new Sequelize(`postgres://${config.username}:${config.password}@${config.host}:${config.port}/${config.database}`, 
+// options);
