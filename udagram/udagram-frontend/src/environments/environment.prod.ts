c8141971876f5c85ad785af55ce2dad86f4b8e66
Ksenya <ks_s_6@mail.ru>
2023-09-03 17:18:59 +0200

    Update8

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api2-dev.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-api2-dev2.us-east-1.elasticbeanstalk.com/api/v0",
   //apihost: 'http://localhost:8080/api/v0'
 };
 
