e750a9567800497ef9302f142e8a5b668be668ee
Ksenya <ks_s_6@mail.ru>
2023-09-03 19:49:37 +0200

    Update16

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,25 +1,18 @@
-import { Sequelize, SequelizeOptions  } from "sequelize-typescript";
+import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-const options: SequelizeOptions = {
+export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
   database: config.database,
   host: config.host,
-  port: config.port,
+
   dialect: "postgres",
   dialectOptions: {
     ssl: {
-      require: true, 
-      rejectUnauthorized: false 
+        "require": true,
+        "rejectUnauthorized": false
     }
   },
   storage: ":memory:",
-  logging: console.log
-};
-
-console.log(config);
-
-
-export const sequelize = new Sequelize(`postgres://${config.username}:${config.password}@${config.host}:${config.port}/${config.database}`, 
-options);
+});
--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -18,28 +18,32 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
     console.error("Unable to connect to the database:", error);
   }
 
-  try {
-    await sequelize.addModels(V0_FEED_MODELS);
-    await sequelize.addModels(V0_USER_MODELS);
-    console.log('Models were added');
-    await sequelize.sync({ force: true, logging: console.log });
-    console.log("Database connected");
-  }
-  catch (error) {
-    console.log(error)
-    console.error("Unable to connect to the database:", error);
-  }
+  await sequelize.addModels(V0_FEED_MODELS);
+  await sequelize.addModels(V0_USER_MODELS);
+  await sequelize.sync();
 
+  console.log("Database Connected");
 
   const app = express();
   const port = 8080;
 
   app.use(bodyParser.json());
 
-  //app.use(cors());
+  // app.use(cors());
   // We set the CORS origin to * so that we don't need to
   // worry about the complexities of CORS. 
-  app.use(cors());
+  app.use(cors({
+    "allowedHeaders": [
+      'Origin', 'X-Requested-With',
+      'Content-Type', 'Accept',
+      'X-Access-Token', 'Authorization', 'Access-Control-Allow-Origin',
+      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Methods'
+    ],
+    "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    "preflightContinue": true,
+    "origin": '*',
+  }));
 
   app.use("/api/v0/", IndexRouter);
 
@@ -51,7 +55,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   // Start the Server
   app.listen(port, () => {
     console.log(`Backend server is listening on port ${port}....`);
- //   console.log(`Frontent server running ${process.env.URL} port ${port}`);
+    console.log(`Frontent server running ${process.env.URL}`);
     console.log(`press CTRL+C to stop server`);
   });
 })();
